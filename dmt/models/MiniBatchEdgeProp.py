b4a917e6d7477dd9b347c18078507e4d113febf4
Handason <handasontam@gmail.com>
2019-05-20 00:59:37 +0800

    bug fix

--- dmt/models/MiniBatchEdgeProp.py
+++ dmt/models/MiniBatchEdgeProp.py
@@ -36,38 +36,6 @@ class OneLayerNN(nn.Module):
         h = F.relu(h)
         return h
 
-class TwoLayerNN(nn.Module):
-    def __init__(self,
-                 in_dim,
-                 hidden,
-                 out_dim,
-                 feat_drop,
-                 last=False,
-                 **kwargs):
-        super(TwoLayerNN, self).__init__(**kwargs)
-        self.last = last
-        if feat_drop:
-            self.feat_drop = nn.Dropout(feat_drop)
-        else:
-            self.feat_drop = lambda x : x
-        self.fc = nn.Linear(in_dim, hidden, bias=True)
-        self.fc2 = nn.Linear(hidden, out_dim, bias=True)
-        self.layer_norm1 = nn.LayerNorm(normalized_shape=hidden)
-        if not self.last:
-            self.layer_norm2 = nn.LayerNorm(normalized_shape=out_dim)
-
-    def forward(self, h):
-        h = self.fc(h)
-        h = self.layer_norm1(h)
-        h = F.relu(h)
-        h = self.feat_drop(h)
-        h = self.fc2(h)
-        if not self.last:
-            h = self.layer_norm2(h)
-            h = F.relu(h)
-        return h
-
-
 class NodeUpdate(nn.Module):
     def __init__(self, layer_id, in_dim, out_dim, feat_drop,
                  test=False, last=False, name=''):
@@ -82,11 +50,6 @@ class NodeUpdate(nn.Module):
         self.layer = OneLayerNN(in_dim=in_dim, 
                                 out_dim=out_dim, 
                                 last=last)
-        # self.layer = TwoLayerNN(in_dim=in_dim, 
-        #                         hidden=hidden, 
-        #                         out_dim=out_dim, 
-        #                         feat_drop=feat_drop, 
-        #                         last=last)
         self.name = name
 
     def forward(self, node):
@@ -104,23 +67,24 @@ class NodeUpdate(nn.Module):
         if self.test:
             # average
             h = (h - self_h) * norm
-            # graphsage
-            h = torch.cat((h, self_h), 1)
         else:
-            agg_history_str = 'agg_history_{}'.format(self.layer_id)
-            agg_history = node.data[agg_history_str]
             # normalization constant
             subg_norm = node.data['subg_norm']
-            # delta_h (h - history) from previous layer of myself
-            self_delta_h = node.data['self_delta_h']
-            # control variate for variance reduction
-            # h-self_delta_h because:
-            # {1234} -> {34}
-            # we only want sum of delta_h for {1,2}
-            h = (h - self_delta_h) * subg_norm + agg_history * norm
-            # graphsage
-            h = torch.cat((h, self_h), 1)
-            h = self.feat_drop(h)
+            if self.layer_id == 0:
+                h = (h - self_h) * subg_norm
+            else:
+                agg_history_str = 'agg_history_{}'.format(self.layer_id)
+                agg_history = node.data[agg_history_str]
+                # delta_h (h - history) from previous layer of myself
+                self_delta_h = node.data['self_delta_h']
+                # control variate for variance reduction
+                # h-self_delta_h because:
+                # {1234} -> {34}
+                # we only want sum of delta_h for {1,2}
+                h = (h - self_delta_h) * subg_norm + agg_history * norm
+        # graphsage
+        h = torch.cat((h, self_h), 1)
+        h = self.feat_drop(h)
 
         h = self.layer(h)
 
@@ -147,24 +111,38 @@ class MiniBatchEdgeProp(nn.Module):
             self.feat_drop = nn.Dropout(feat_drop)
         else:
             self.feat_drop = lambda x : x
+        self.in_dim = in_dim
+        self.edge_in_dim = edge_in_dim
+        self.num_hidden = num_hidden
 
-        self.input_layer = OneLayerNN(in_dim=in_dim, 
-                                      out_dim=num_hidden)
+        # self.input_layer = OneLayerNN(in_dim=in_dim, 
+        #                               out_dim=num_hidden)
 
-        self.phi = OneLayerNN(in_dim=2*num_hidden,
-                              out_dim=num_hidden)
+        # self.phi = OneLayerNN(in_dim=2*num_hidden,
+        #                       out_dim=num_hidden)
 
         self.input_layer_e = OneLayerNN(in_dim=edge_in_dim, 
                                       out_dim=num_hidden)
         self.node_layers = nn.ModuleList()
-        # hidden layers
-        for i in range(num_layers):
-                self.node_layers.append(NodeUpdate(layer_id=i, 
-                                            in_dim=2*num_hidden, 
-                                            out_dim=num_hidden, 
-                                            feat_drop=feat_drop, 
-                                            name='node', 
-                                            test=True))
+        self.node_layers.append(NodeUpdate(layer_id=0, 
+                                        in_dim=2*num_hidden, 
+                                        out_dim=num_hidden, 
+                                        feat_drop=feat_drop, 
+                                        name='node', 
+                                        test=False))
+
+        self.phi = nn.ModuleList()
+        self.phi.append(OneLayerNN(in_dim=in_dim+num_hidden,
+                                   out_dim=num_hidden))
+        for i in range(1, num_layers):
+            self.node_layers.append(NodeUpdate(layer_id=i, 
+                                        in_dim=2*num_hidden, 
+                                        out_dim=num_hidden, 
+                                        feat_drop=feat_drop, 
+                                        name='node', 
+                                        test=False))
+            self.phi.append(OneLayerNN(in_dim=2*num_hidden,
+                                       out_dim=num_hidden))
 
         
         # output projection
@@ -182,10 +160,10 @@ class MiniBatchEdgeProp(nn.Module):
         '''
         nf = nodeflow
         h = nf.layers[0].data['features']
-        h = self.feat_drop(h)
-        h = self.input_layer(h)  # ((#nodes in layer_i) X D)
+        # h = self.feat_drop(h)
+        # h = self.input_layer(h)  # ((#nodes in layer_i) X D)
 
-        for i, node_layer in enumerate(self.node_layers):
+        for i, (node_layer, phi_layer) in enumerate(zip(self.node_layers, self.phi)):
             # compute edge embeddings
             e = nf.blocks[i].data['edge_features']
             e = self.feat_drop(e)
@@ -195,22 +173,28 @@ class MiniBatchEdgeProp(nn.Module):
 
             parent_nid = dgl.utils.toindex(nf.layer_parent_nid(i+1))
             layer_nid = nf.map_from_parent_nid(i, parent_nid)
-            self_h = h[layer_nid]  # ((#nodes in layer_i+1) X D)
-            nf.layers[i+1].data['self_h'] = self_h
-            new_history = h.detach()
-            history_str = 'history_{}'.format(i)
-            history = nf.layers[i].data[history_str]  # ((#nodes in layer_i) X D)
+            self_h = torch.cat((torch.zeros(len(layer_nid), self.num_hidden), h[layer_nid]), 1)
+            self_h = phi_layer(self_h)
+            nf.layers[i+1].data['self_h'] = self_h # ((#nodes in layer_i+1) X D)
+            if i == 0:
+                nf.layers[i].data['h'] = h
+            else:
+                new_history = h.detach()
+                history_str = 'history_{}'.format(i)
+                history = nf.layers[i].data[history_str]  # ((#nodes in layer_i) X D)
 
-            # delta_h used in control variate
-            delta_h = h - history  # ((#nodes in layer_i) X D)
-            # delta_h from previous layer of the nodes in (i+1)-th layer, used in control variate
-            nf.layers[i+1].data['self_delta_h'] = delta_h[layer_nid]
+                # delta_h used in control variate
+                delta_h = h - history  # ((#nodes in layer_i) X D)
+                # delta_h from previous layer of the nodes in (i+1)-th layer, used in control variate
+                nf.layers[i+1].data['self_delta_h'] = delta_h[layer_nid]
 
-            nf.layers[i].data['h'] = delta_h
+                nf.layers[i].data['h'] = delta_h
+
 
             def message_func(edges):
                 temp = torch.cat((edges.data['e'],edges.src['h']), 1)
-                temp = self.phi(temp)
+                temp = phi_layer(temp)
+                temp = self.activation(temp)
                 return {'m': temp}
     
             nf.block_compute(i,
@@ -220,7 +204,7 @@ class MiniBatchEdgeProp(nn.Module):
             h = nf.layers[i+1].data.pop('activation')
 
             # update history
-            if i < nf.num_layers-1:
+            if (i < nf.num_layers-1) and (i!=0):
                 nf.layers[i].data[history_str] = new_history
         h = self.fc(h)
         return h
@@ -251,25 +235,56 @@ class MiniBatchEdgePropInfer(nn.Module):
             self.feat_drop = nn.Dropout(feat_drop)
         else:
             self.feat_drop = lambda x : x
+        self.in_dim = in_dim
+        self.edge_in_dim = edge_in_dim
+        self.num_hidden = num_hidden
+        # self.input_layer = OneLayerNN(in_dim=in_dim, 
+        #                               out_dim=num_hidden)
 
-        self.input_layer = OneLayerNN(in_dim=in_dim, 
-                                      out_dim=num_hidden)
 
-
-        self.phi = OneLayerNN(in_dim=2*num_hidden,
-                              out_dim=num_hidden)
+        # self.phi = OneLayerNN(in_dim=2*num_hidden,
+        #                       out_dim=num_hidden)
 
         self.input_layer_e = OneLayerNN(in_dim=edge_in_dim, 
                                       out_dim=num_hidden)
         self.node_layers = nn.ModuleList()
-        # hidden layers
-        for i in range(num_layers):
-                self.node_layers.append(NodeUpdate(layer_id=i, 
-                                            in_dim=2*num_hidden, 
-                                            out_dim=num_hidden, 
-                                            feat_drop=feat_drop, 
-                                            name='node', 
-                                            test=True))
+        self.node_layers.append(NodeUpdate(layer_id=0, 
+                                        in_dim=2*num_hidden, 
+                                        out_dim=num_hidden, 
+                                        feat_drop=feat_drop, 
+                                        name='node', 
+                                        test=True))
+
+        self.phi = nn.ModuleList()
+        self.phi.append(OneLayerNN(in_dim=in_dim+num_hidden,
+                                   out_dim=num_hidden))
+        for i in range(1, num_layers):
+            self.node_layers.append(NodeUpdate(layer_id=i, 
+                                        in_dim=2*num_hidden, 
+                                        out_dim=num_hidden, 
+                                        feat_drop=feat_drop, 
+                                        name='node', 
+                                        test=True))
+            self.phi.append(OneLayerNN(in_dim=2*num_hidden,
+                                       out_dim=num_hidden))
+        # self.input_layer_e = OneLayerNN(in_dim=edge_in_dim, 
+        #                               out_dim=num_hidden)
+        # self.node_layers = nn.ModuleList()
+        # self.phi = nn.ModuleList()
+        # # hidden layers
+        # for i in range(num_layers):
+        #     self.node_layers.append(NodeUpdate(layer_id=i, 
+        #                                 in_dim=2*num_hidden, 
+        #                                 out_dim=num_hidden, 
+        #                                 feat_drop=feat_drop, 
+        #                                 name='node', 
+        #                                 test=True))
+        # self.phi.append(OneLayerNN(in_dim=in_dim+num_hidden,
+        #                            out_dim=num_hidden))
+        # for i in range(1, num_layers):
+        #     self.phi.append(OneLayerNN(in_dim=2*num_hidden,
+        #                                out_dim=num_hidden))
+
         
         # output projection
         self.fc = nn.Linear(num_hidden, num_classes, bias=True)
@@ -286,35 +301,38 @@ class MiniBatchEdgePropInfer(nn.Module):
         '''
         nf = nodeflow
         h = nf.layers[0].data['features']
-        h = self.feat_drop(h)
-        h = self.input_layer(h)  # ((#nodes in layer_i) X D)
+        # h = self.feat_drop(h)
+        # h = self.input_layer(h)  # ((#nodes in layer_i) X D)
 
-        for i, node_layer in enumerate(self.node_layers):
+        for i, (node_layer, phi_layer) in enumerate(zip(self.node_layers, self.phi)):
             # compute edge embeddings
             e = nf.blocks[i].data['edge_features']
             e = self.feat_drop(e)
             e = self.input_layer_e(e)
             nf.blocks[i].data['e'] = e
             
-            nf.layers[i].data['h'] = h
             parent_nid = dgl.utils.toindex(nf.layer_parent_nid(i+1))
             layer_nid = nf.map_from_parent_nid(i, parent_nid)
-            self_h = h[layer_nid]  # ((#nodes in layer_i+1) X D)
-            nf.layers[i+1].data['self_h'] = self_h
+            self_h = torch.cat((torch.zeros(len(layer_nid), self.num_hidden), h[layer_nid]), 1)
+            self_h = phi_layer(self_h)
+            nf.layers[i+1].data['self_h'] = self_h # ((#nodes in layer_i+1) X D)
 
+            if i == 0:
+                nf.layers[i].data['h'] = h
             def message_func(edges):
                 temp = torch.cat((edges.data['e'],edges.src['h']), 1)
-                temp = self.phi(temp)
+                temp = phi_layer(temp)
+                temp = self.activation(temp)
                 return {'m': temp}
 
             nf.block_compute(i,
                             message_func,
                             fn.sum(msg='m', out='h'), 
                             node_layer)
-            embeddings = nf.layers[i+1].data.pop('activation')
+            h = nf.layers[i+1].data.pop('activation')
 
-        h = self.fc(embeddings)
-        return h, embeddings
+        embeddings = h
+        return self.fc(h), embeddings
 
     def edge_nonlinearity(self, edges):
         eft = self.activation(edges.data['eft'])
